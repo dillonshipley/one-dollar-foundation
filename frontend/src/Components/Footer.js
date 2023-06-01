736951af91a2716f94a50866f44dd6b6c992b068
dillonshipley <dillonshipley@proton.me>
2023-05-31 22:38:25 -0700

    lots of changes. making this thing MOBILE
    front end!

--- frontend/src/Components/Footer.js
+++ frontend/src/Components/Footer.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {FadeImage} from './FadeImage'
 
 export default class Footer extends React.Component {
+  componentDidMount(){
+    console.log("lit");
+  }
+
     render(){
       return(
         <div>
           <div className = "footer">
-            <div className = 'icons'>
+            <div className = 'footerIcons'>
               <FadeImage url = 'youtube'/>
               <FadeImage url = 'instagram' />
               <FadeImage url = 'discord'/>
