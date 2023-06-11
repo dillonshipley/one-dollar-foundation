15effe1872fbc7303ce722953f03eb34c8352b6c
dshipley <dillonshipley@proton.me>
2023-06-11 11:13:44 -0700

    theres still an error but I want to push to main

--- frontend/src/Components/Homepage/Homepage.js
+++ frontend/src/Components/Homepage/Homepage.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../../css/SocialDropdown.css'
+import SocialDropdown from './SocialDropdown';
+import Subscribe from './Subscribe';
 
 function DonationButton({click}){
     return(
@@ -10,94 +11,37 @@ function DonationButton({click}){
       </div>
     );
   }
-
-function Subscribe(){
-    return (
-        <div className = "subscribeContainer">
-            <div className = "subscribe">
-                <div className = "c">
-                  <div className = "subscribeHeader">Subscribe to see how we use every dollar donated.</div>
-                </div>
-                <form>
-                    <label htmlFor ="email" className = "st input label">Email:</label>
-                    <input id = "email" className = "st input email" placeholder = "Enter your email here">
-                        
-                    </input>
-                    <label htmlFor ="suggestion label">Suggestion:</label>
-                    <div className = "suggestionContainer input st suggestion">
-                      <div className = "suggestionOverlay">Please let us know of any events in your community you'd like us to be involved in</div>
-                      <input className = "st input suggestion"></input>
-
-                    </div>            
-                </form>
-                <div className = "moreSubText c">
-                  <p className = "st email">We will <u>only</u> email you once a month for our newsletter and expenditures report.</p>
-                  <p className = "st suggestion">We will never send you email asking for money or personal information.</p>
-                  <div className = "subscribeButton">Subscribe</div>
-                </div>
-            </div>
-        </div>
-    );
-  }
   
 
 function Background(){
     return (
         <div className="homepageImageContainer">
-            <div className="mainBackground hpSize">
-                <img src={process.env.PUBLIC_URL + '/images/background.jpeg'} alt="background" className = "bgImage"/>
+            <div className="mainBackground hpSize s1height">
+                <img src={process.env.PUBLIC_URL + '/images/background.jpeg'} alt="background" className = "bgImage s1height"/>
             </div>
-            <div className = "mainBackground hpSize darken" />
+            <div className = "mainBackground hpSize s1height darken" />
       </div>
     );
 }
 
-function DropdownOption({type}){
-  var href = "";
-  switch(type){
-    case 'Instagram':
-      href = 'https://www.instagram.com/_alivanov__/'
-      break;
-    case 'YouTube':
-      href = 'https://www.youtube.com/@alivanov247';
-      break;
-    case 'Discord':
-      href = 'https://discord.com/invite/Pge52B5U';
-      break;
-    default:
-      console.log('Error!')
-      break;
-  }
-
-  return (
-    <a href={href} className = 'SocialDropdownOption'>
-      <img className = 'socialOption' src={process.env.PUBLIC_URL + '/images/' + type + 'Logo.png'}/>
-    </a>
-    );
-}
-
-function SocialDropdown({dropdown}){
-  return (
-    <div className = {`SocialDropdown ${dropdown ? 'slide-down' : 'slide-up'}`}>
-      <DropdownOption type = 'Discord'/>
-      <DropdownOption type = 'Instagram'/>
-      <DropdownOption type = 'YouTube' />
+function Overlay({modal}){
+  return(
+    <div className = 'overlayContainer'>
+      <div className ="overlay o1">
+        <p>Changing the world one step at a time.</p>
+      </div>
+      <p className="overlay o2">Welcome to the One Dollar Foundation.</p>
+      <DonationButton click = {modal}/>
     </div>
-  );
+);
 }
 
 export default function HomepageImage({modal, expanded}){
     return (
-      <div className = 'homepageContainer'>
+      <div className = 'homepageContainer s1height'>
         <Background />
-        <div className = {window.innerWidth > 1200 ? "hp desktopHomepage" : "hp mobileHomepage"}>
-          <div className = 'overlayContainer'>
-            <div className ="overlay o1">
-              <p>Changing the world one step at a time.</p>
-            </div>
-            <p className="overlay o2">Welcome to the One Dollar Foundation.</p>
-            <DonationButton click = {modal}/>
-          </div>
+        <div className = {window.innerWidth > 1200 ? "hp s1height desktopHomepage" : "hp s1height mobileHomepage"}>
+          <Overlay modal = {modal} />
           {window.innerWidth > 1200 && <Subscribe />}
         </div>
         <SocialDropdown dropdown = {expanded} />
